225996901a841872f87b2c0747f05858a39d3346
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-10 07:28:37 +0530

    Added : Three_Inputs

---
+++ Arthmetic_opration.sh
@@ -0,0 +1,9 @@
+#!/bin/bash  
+
+echo "Welcome Arithmetic Computation And Sorting"
+
+echo "To Take Three Inputs"
+
+read -p "Enter first number  - " a
+read -p "Enter first number  - " b
+read -p "Enter first number  - " c
