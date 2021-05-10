4bb0332bef41a207a151da499f0538a91715e092
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-10 08:16:59 +0530

    Added : Sort_in_accending_order

--- Arthmetic_opration.sh
+++ Arthmetic_opration.sh
@@ -58,3 +58,20 @@ do
 done
 
 echo "Sorted Array -" ${array[@]}
+
+echo "Sort The Computed Results In Ascending Order"
+
+for(( i=0; i<${#array[@]}; i++ ))
+do
+        for(( j=0; j<${#array[@]}-1; j++ ))
+        do
+                if [[ ${array[j+1]} -lt ${array[j]} ]]
+                then
+                        temp=${array[j]}
+                        array[j]=${array[j+1]}
+                        array[j+1]=$temp
+                fi
+        done
+done
+
+echo "Sorted Array -" ${array[@]}
